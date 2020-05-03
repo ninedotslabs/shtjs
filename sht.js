f4a9d0362f251766d219cb2ec799373fc6f28a44
alfianguide <alfianguide@outlook.com>
2020-05-03 21:04:05 +0700

    add jsx support and representation

--- sht.js
+++ sht.js
@@ -4,6 +4,7 @@
  * Copyright Alfian Hidayat.
  * there's must be only one parameters!
  * This is a module, please learn how to use it ok?
+ * Good artist copy, great artist STEAL
  */
 
 // debug
@@ -26,5 +27,47 @@ export const elGetAtr = el => atr => el.elGetAtr(atr);
 export const elSetAtr = el => atr => a => el.setAttribute(atr, a);
 export const elDelAtr = el => atr => el.deleteAttribute(atr);
 export const elTxt = el => txt => (el.text = txt);
+export function render(element, container) {
+  if (typeof element.type === "function") element = element.type();
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+  const isProperty = key => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name];
+    });
+  element.props.children?.forEach(child => render(child, dom));
+  container.appendChild(dom);
+}
+export function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  };
+}
+export function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object" ? child : createTextElement(child)
+      )
+    }
+  };
+}
+export function injectStyle(style) {
+    let styleCont = document.createElement("style")
+    styleCont.append(style)
+    return document.body.append(styleCont)
+}
 // unique
 export const setTitle = t => elTxt(elQry("title"))(t);
+
+export const Shtjs = { render, createElement, createTextElement, injectStyle }
--- test.js
+++ test.js
@@ -1,6 +1,73 @@
-import { out, sum, sub, mul, div, mod  } from "./sht.js";
+import { Shtjs, out, sum, sub, mul, div, mod  } from "./sht.js";
 out("Sum " + sum(10)(5));
 out("Sub " + sub(10)(5));
 out("Mult " + mul(10)(5));
 out("Div " + div(10)(5));
 out("Mod " + mod(10)(5));
+
+function Menu() {
+    let fruits = ["mango","apple","watermelon"];
+    const menuList = [
+	...fruits.map(f => {
+		let propsMenuItem = {
+		    className: "Menu__Item"
+		}
+		return Shtjs.createElement("li", propsMenuItem, f)
+	    }
+	)
+    ]
+    let propsMenu = {
+	className: "Menu"
+    }
+    return Shtjs.createElement("ul", propsMenu, ...menuList)  
+}
+
+function Greet() {
+    return Shtjs.createElement("h1", null, "Hello and Welcome! this is representation of Shtjs")
+}
+
+function Foot() {
+    let propsFoot = {
+	className: "Footer"
+    }
+    return Shtjs.createElement("p", propsFoot, "Copyright (c) 2020 Alfian Hidayat")
+}
+
+function App() {
+    let propsApp = {
+	className: "App"
+    }
+    let appChildren = [Menu(), Greet(), Foot()]
+    return Shtjs.createElement("div", propsApp, ...appChildren); 
+}
+
+let container = document.getElementById("root");
+
+let styles = `
+.App {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 50rem;
+}
+.Footer {
+    padding: 10px;
+    background-color: #000000;
+    color: #ffffff;
+}
+.Menu {
+    list-style-type: none;
+    display: flex;
+    padding: 0px;
+    margin: 0px;
+}
+.Menu__Item {
+    margin-right: 5px;
+    padding: 15px;
+    background-color: #000000;
+    color: #ffffff;
+}
+`
+
+Shtjs.injectStyle(styles)
+
+Shtjs.render(App(), container)
